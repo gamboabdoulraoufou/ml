4315f684345ba3f9e55ad4d09c467ab3a164c517
Abdoul <gamboabdoulraouf@gmail.com>
2015-01-26 01:05:12 +0100

    Update r_script.R

--- r_script.R
+++ r_script.R
@@ -1,7 +1,8 @@
 
 # Import libraries
-library(caret)
-library(ggplot2)
+require(caret)
+require(ggplot2)
+require(randomForest)
 
 # Load data
 # setwd(dir="/home/abdoul/git/ml/")
@@ -9,12 +10,32 @@ training <- read.csv("pml-training.csv", sep=",")
 test <- read.csv("pml-testing.csv", sep=",")
 
 
-# Modeling
-model <- train(classe ~., data=training, metho="rf", prox=TRUE)
-model
+# Select predictors
+training <- training[,7:160]
+test <- test[,7:160]
 
-# prediction
-pred <- predict(model, test)
-test$predRigh
-table(pred,test$classe)
+# Remove predictors that have most NA values
+v <- apply(!is.na(training),2,sum)>19621
+training <- training[,v]
+test <- test[,v]
+
+# Sample
+inTrain <- createDataPartition(y=training$classe,p=0,6,list=FALSE)
+training_train <- training[inTrain, ]
+training_test <- training[-inTrain, ]
+
+# Modeling
+model <- train(classe ~., data=training_train, metho="rf", prox=TRUE)
+
+# Validation on test sample
+pred <- predict(model, training_test)
+table(pred,training_test$classe)
+
+# Print and cross validation accuracy
+print(model)
+print(model$finalModel)
+
+# prediction on test data
+prediction <- predict(model, test)
+table(prediction$classe)
 
